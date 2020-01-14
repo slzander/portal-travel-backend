8dc8618d23da5b07240984079313cdf0f48b7058
slzander <staceyzander@gmail.com>
2020-01-14 10:17:47 -0700

    changes user and image ids for seed file

--- seeds/03_user_images.js
+++ seeds/03_user_images.js
@@ -2,9 +2,9 @@ exports.seed = function(knex) {
   return knex('user-images').del()
     .then(function () {
       return knex('user-images').insert([
-        { user_id: 1, image_id: 28},
-        { user_id: 1, image_id: 29},
-        { user_id: 1, image_id: 30}
+        { user_id: 8, image_id: 31},
+        { user_id: 8, image_id: 29},
+        { user_id: 8, image_id: 30}
       ]);
     });
 };
