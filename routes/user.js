aa7e5b9d781f37f4273bbaa7cc50793cc64b57f3
slzander <staceyzander@gmail.com>
2020-01-15 21:25:14 -0700

    Revert "adds auth routes and queries"
    This reverts commit 269657c3cece4d98fae0d402f2865e1ca1466762.

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const queries = require('../db/queries')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const secret = 'shhdonttell'
 
 router.get('/user', (request, response) => {
     queries
@@ -17,40 +14,8 @@ router.post('/user', (request, response) => {
     queries
         .users
         .create(request.body)
-        .then(user => {
-            if(!user){
-                response.status(401).json({ error:'Wrong username/password' })
-            } else {
-                jwt.sign(user, secret, (error, token) => {
-                    if(error) trhow new Error(error)
-                    response.status(201).json({ token })
-                })
-            }
-        })
-        .catch(error => response.status(401).json({ error: error.message }))
-})
-
-router.post('/login', (request, response) => {
-    queries
-        .login
-        .authorizeUser(request.body)
-        .then(user => {
-            if(!user){
-                response.status(401).json({ error:'Wrong username/password' })
-            } else {
-                return bcrypt
-                    .compare(request.body.password, user.password_digest)
-                    .then(isAuthenticated => {
-                        if(!isAuthenticated){
-                            response.status(401).json({ error: 'Wrong username/password'})
-                        } else {
-                            return jwt.sign(user, secret, (error, token) => {
-                                response.status(200).json({ token })
-                            })
-                        }
-                    })
-            }
-        })
+        .then(results => response.send(results))
+        .then(response.status(201))
 })
 
 router.delete('/user/:id', (request, response) => {
